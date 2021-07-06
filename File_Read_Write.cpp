cbfb3b2f76b9dbce747c421df45875f66ce1b784
Md. Farhan Sadiq <42261830+mdfarhansadiq@users.noreply.github.com>
2021-07-07 00:07:08 +0600

    Create File_Read_Write.cpp

---
+++ File_Read_Write.cpp
@@ -0,0 +1,24 @@
+///******* In the name of Allah *******\\\
+
+#include <bits/stdc++.h>
+using namespace std;
+
+void Ok()
+{
+    long int a,b,c,d,e,f,g,h,t,i,j,k,l,m,n,Mn,Mx;   ///from line 7 to line 11 are variables. 
+    long int ans1,ans2,ans3,cnt1,cnt2,cnt3,flag1,flag2,flag3,temp,sum;
+    double aa,bb,cc,dd,ee,ff,gg;
+    char aaa,bbb,ccc,ddd;
+    string spc,in,store;
+
+    return;
+}
+
+int main()
+{
+    freopen("input.txt","r",stdin);     ///This line is for file read
+    freopen("output.txt","w",stdout);   ///This line is for file write
+    Ok();
+
+    return 0;
+}
