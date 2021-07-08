c77ef5843a667932fd25fb2f4ef195363996521e
Md. Farhan Sadiq <42261830+mdfarhansadiq@users.noreply.github.com>
2021-07-08 18:48:54 +0600

    Update File_Read_Write.cpp

--- File_Read_Write.cpp
+++ File_Read_Write.cpp
@@ -5,7 +5,7 @@ using namespace std;
 
 void Ok()
 {
-    long int a,b,c,d,e,f,g,h,t,i,j,k,l,m,n,Mn,Mx;   ///from line 7 to line 11 are variables. 
+    long int a,b,c,d,e,f,g,h,t,i,j,k,l,m,n,Mn,Mx;   ///from line 8 to line 11 are variables. 
     long int ans1,ans2,ans3,cnt1,cnt2,cnt3,flag1,flag2,flag3,temp,sum;
     double aa,bb,cc,dd,ee,ff,gg;
     char aaa,bbb,ccc,ddd;
